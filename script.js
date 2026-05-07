56fc38842a7bdde101b86d5ad2f84925bc7c67bd
Jose Garcia <sosachango1@gmail.com>
2026-05-07 02:50:52 -0500

    last update js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-// const menuBtn = document.getElementById("menu-btn");
-// const xBtn = document.getElementById("x-btn")
-// const nav = document.getElementById("nav");
 
-// menuBtn.addEventListener("click", () => {
-//     nav.classList.toggle("show");
-// });
-
-// xBtn.addEventListener("click", () =>{
-//     nav.classList.toggle('hidden')
-// })
 
 const menu= document.querySelector(".menu-btn");
 const nav = document.querySelector(".nav_bar");
